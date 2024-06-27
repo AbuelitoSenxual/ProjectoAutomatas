add7264b98d45b3d0d1f246c0260ff7cd5e7e6dd
Marco <MarcoDavi67@hotmail.com>
2024-06-27 10:30:14 -0700

    Nodo Identificadores

---
+++ AnalizadorLexico y sintactico 2.0/src/NodoIdentificadores.java
@@ -0,0 +1,16 @@
+public class NodoIdentificadores {
+    String Nombre;
+    String TipoDato;
+    Object Valor;
+    NodoIdentificadores SigIdentificador;
+    
+    
+    public NodoIdentificadores (String Nombre,String TipoDato,Object Valor){
+        this.Nombre = Nombre;
+        this.TipoDato = TipoDato;
+        this.Valor = Valor;
+    
+    }
+}
+
+
